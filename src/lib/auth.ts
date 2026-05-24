45c86716b850b46166b7e5617a4c60a493440eee
bobby-brown96 <bobby.myfinancesnow@gmail.com>
2026-05-24 01:58:30 -0500

    better auth start

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,14 @@
-import { betterAuth } from 'better-auth'
-import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { betterAuth } from "better-auth";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "#/db";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
   plugins: [tanstackStartCookies()],
-})
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+});
